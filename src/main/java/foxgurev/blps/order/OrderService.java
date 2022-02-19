769f8b2df607a98c61b9a00fb0ec701fd6138bc7
nikita <notgurev@gmail.com>
2022-02-19 10:23:01 +0300

    fix percent

--- src/main/java/foxgurev/blps/order/OrderService.java
+++ src/main/java/foxgurev/blps/order/OrderService.java
@@ -1,11 +1,14 @@
 package foxgurev.blps.order;
 
+import foxgurev.blps.Startup;
 import foxgurev.blps.exceptions.NotFoundException;
 import foxgurev.blps.product.Product;
 import foxgurev.blps.product.ProductRepository;
 import foxgurev.blps.promocode.InactivePromocodeException;
 import foxgurev.blps.promocode.Promocode;
 import foxgurev.blps.promocode.PromocodeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,8 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final PromocodeService promocodeService;
     private final ProductRepository productRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
     @Autowired
     public OrderService(OrderRepository orderRepository, PromocodeService promocodeService, ProductRepository productRepository) {
@@ -32,9 +37,8 @@ public class OrderService {
         List<Product> items = productRepository.findAllById(ocr.getProducts());
         Integer sum = items.stream().map(Product::getPrice).reduce(Integer::sum).orElseThrow(() -> {
             throw new RuntimeException("Failed to calculate sum of order");
-        }) * (100 - promocode.getDiscount());
+        }) * (100 - promocode.getDiscount()) / 100;
 
-//        System.out.println(ocr);
         Order o = new Order(OrderStatus.CREATED, items, promocode, sum,
                 ocr.name, ocr.surname, ocr.phoneNumber, ocr.email, ocr.city);
         orderRepository.save(o);
