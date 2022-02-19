af6ff1bd24046c7ec8b087c280b29f6df3c2ccd9
nikita <notgurev@gmail.com>
2022-02-19 10:38:36 +0300

    fix order reserved word

--- src/main/java/foxgurev/blps/order/Order.java
+++ src/main/java/foxgurev/blps/order/Order.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "orderr")
 public class Order {
     @Id
     @GeneratedValue
--- src/main/java/foxgurev/blps/product/Product.java
+++ src/main/java/foxgurev/blps/product/Product.java
@@ -3,7 +3,10 @@ package foxgurev.blps.product;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @Entity
