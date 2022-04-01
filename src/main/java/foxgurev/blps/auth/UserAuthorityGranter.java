47146f9f32e9ad86751f6133661b47a861d6aa1f
nikita <notgurev@gmail.com>
2022-04-02 02:23:33 +0300

    add logging to UserAuthorityGranter

--- src/main/java/foxgurev/blps/auth/UserAuthorityGranter.java
+++ src/main/java/foxgurev/blps/auth/UserAuthorityGranter.java
@@ -2,6 +2,7 @@ package foxgurev.blps.auth;
 
 import foxgurev.blps.auth.user.Role;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.jaas.AuthorityGranter;
 
 import java.security.Principal;
@@ -10,13 +11,16 @@ import java.util.Optional;
 import java.util.Set;
 
 @RequiredArgsConstructor
+@Slf4j
 public class UserAuthorityGranter implements AuthorityGranter {
     private final UserRepository userRepository;
 
     @Override
     public Set<String> grant(Principal principal) {
-        Optional<Role> optionalRole = userRepository.findRoleByEmail(principal.getName());
+        String email = principal.getName();
+        Optional<Role> optionalRole = userRepository.findRoleByEmail(email);
         Role role = optionalRole.orElseThrow(() -> new RuntimeException("Failed to find role of user by email"));
-        return Collections.singleton(String.valueOf(role));
+        log.info("Granting role " + role.name() + " to " + email);
+        return Collections.singleton(role.name());
     }
 }
