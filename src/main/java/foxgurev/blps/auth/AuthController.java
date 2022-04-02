11f5e3218a08839c78d0a2ce42e2aee575d6ef02
nikita <notgurev@gmail.com>
2022-04-02 03:21:03 +0300

    AuthController cleanup

--- src/main/java/foxgurev/blps/auth/AuthController.java
+++ src/main/java/foxgurev/blps/auth/AuthController.java
@@ -7,58 +7,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static org.springframework.http.ResponseEntity.status;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class AuthController { // todo cleanup
+public class AuthController {
     private final UserService userService;
 
     @PostMapping(value = "/register")
-//    @PreAuthorize("permitAll()")
-    public ResponseEntity<String> register(@Valid @RequestBody UserDto user, BindingResult result) {
-        log.debug(String.valueOf(user));
-        try {
-            log.info("POST request to register user {}", user);
-            if (result.hasErrors()) {
-                log.info("Validation Error");
-                return new ResponseEntity<>("Validation Error", HttpStatus.BAD_REQUEST);
-            }
-
-            boolean isSaved = userService.saveUser(user);
-            return isSaved ? new ResponseEntity<>("User registered successfully!", HttpStatus.OK) :
-                    new ResponseEntity<>("Email is taken!", HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            log.info("Unexpected Error {}", e.getMessage());
-            return new ResponseEntity<>("Validation Error", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<?> register(@Valid @RequestBody UserDto user) {
+        log.info("POST request to register user {}", user);
+        boolean ok = userService.saveUser(user);
+        return ok ? status(HttpStatus.OK).body("User registered successfully!")
+                : status(HttpStatus.BAD_REQUEST).body("Email is taken!");
     }
 
     @PostMapping(value = "/login")
-//    @PreAuthorize("permitAll()")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult) {
-        log.debug(String.valueOf(loginRequest));
-        try {
-            log.debug("POST request to login user {}", loginRequest);
-
-            if (bindingResult.hasErrors()) {
-                log.error("Validation error");
-                return new ResponseEntity<>("Ошибка валидации", HttpStatus.BAD_REQUEST);
-            }
-
-            LoginResponse loginResponse = userService.login(loginRequest);
-            return new ResponseEntity<>(loginResponse, HttpStatus.OK);
-
-        } catch (Exception e) {
-            log.error("Unexpected error {}", e.getMessage());
-            return new ResponseEntity<>("Неверные учетные данные пользователя", HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+        log.debug("POST request to login user {}", loginRequest);
+        LoginResponse loginResponse = userService.login(loginRequest);
+        return status(HttpStatus.OK).body(loginResponse);
     }
 }
