be1ef0eccc1d746d937893ce59728241d96a5e45
nikita <notgurev@gmail.com>
2022-04-29 00:12:04 +0300

    deserializer: trust all packages

--- greeting-service/src/main/java/foxgurev/services/greeting/GreetingServiceApplication.java
+++ greeting-service/src/main/java/foxgurev/services/greeting/GreetingServiceApplication.java
@@ -3,7 +3,6 @@ package foxgurev.services.greeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
